86c5ef1e8fdb5c12b4b66837a2ece9688652118e
NemoNology <chicken.gang@mail.ru>
2023-09-25 01:10:58 +0900

    Renamed

---
+++ Lab-s/1/Console-Project/Extensions.cs
@@ -0,0 +1,55 @@
+namespace Console_Project
+{
+    public static class Extensions
+    {
+        public static void PrintColored<T>(
+            this IEnumerable<T> str,
+            ConsoleColor foregrounColor = ConsoleColor.White,
+            ConsoleColor backgroundColor = ConsoleColor.Black,
+            bool isWriteLine = false
+        )
+        {
+            Console.BackgroundColor = backgroundColor;
+            Console.ForegroundColor = foregrounColor;
+            var end = isWriteLine ? Environment.NewLine : "";
+            Console.Write(str + end);
+            Console.ResetColor();
+        }
+
+        public static List<int> FindAllIndexOf(this string str, string searchedValue)
+        {
+            List<int> indices = new();
+            var n = searchedValue.Length;
+            var len = str.Length - n;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (str[i..].StartsWith(searchedValue))
+                {
+                    indices.Add(i);
+                }
+            }
+
+            return indices;
+        }
+
+        public static List<int> FindAllIndexOf<T>(this IEnumerable<T> chain, T searchedValue)
+        {
+            List<int> indices = new();
+            var entor = chain.GetEnumerator();
+            var i = 0;
+
+            while (entor.MoveNext())
+            {
+                if (entor.Current!.Equals(searchedValue))
+                {
+                    indices.Add(i);
+                }
+
+                i++;
+            }
+
+            return indices;
+        }
+    }
+}
