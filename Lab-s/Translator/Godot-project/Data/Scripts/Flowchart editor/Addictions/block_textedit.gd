c2ccd5ad25e117c29f2110104c29557be4f9b587
NemoNology <chicken.gang@mail.ru>
2023-12-05 15:36:25 +0900

    Improved flowchart block textEdit class - B 1.5

--- Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/Addictions/block_textedit.gd
+++ Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/Addictions/block_textedit.gd
@@ -8,44 +8,56 @@ var _minHeight: int
 var _font: Font
 
 func _init(
-	placeHolderText: String = "Код сюда!",
+	startText: String = "",
+	placeHolderText: String = "",
 	isEditable: bool = true,
 	maxWidth: int = 350,
-	minWidth: int = 60,
-	minHeight: int = 30):
-		_minWidth = minWidth
-		_minHeight = minHeight
-		_maxWidth = maxWidth
-		custom_minimum_size = Vector2(_minWidth, _minHeight)
-		scroll_fit_content_height = true
-		placeholder_text = placeHolderText
-		wrap_mode = TextEdit.LINE_WRAPPING_BOUNDARY
-		var emptyStyleBox: StyleBoxEmpty = StyleBoxEmpty.new()
-		add_theme_stylebox_override("normal", emptyStyleBox)
-		add_theme_stylebox_override("focus", emptyStyleBox)
-		text_changed.connect(_on_text_changed)
-		_font = get_theme_font("font")
-		editable = isEditable
+	minWidth: int = 80,
+	minHeight: int = 30
+	):
+	_minWidth = minWidth
+	_minHeight = minHeight
+	_maxWidth = maxWidth
+	custom_minimum_size = Vector2(_minWidth, _minHeight)
+	scroll_fit_content_height = true
+	placeholder_text = placeHolderText
+	wrap_mode = TextEdit.LINE_WRAPPING_BOUNDARY
+	var emptyStyleBox: StyleBoxEmpty = StyleBoxEmpty.new()
+	add_theme_stylebox_override("normal", emptyStyleBox)
+	add_theme_stylebox_override("focus", emptyStyleBox)
+	add_theme_stylebox_override("read_only", emptyStyleBox)
+	text_changed.connect(_on_text_changed)
+	add_theme_color_override("color", Color.WHITE)
+	_font = get_theme_font("font")
+	editable = isEditable
+	text = startText
 	
 func _on_text_changed() -> void:
 	var lineCount = get_line_count()
+	var sizeBefore = size
 	# Change text edit width
 	if lineCount > 0:
 		# Finding the biggest line
-		var maxLengthLineLength: int = len(get_line(0))
+		var maxLengthLineLength: float = 0
 		var maxLengthLineIndex: int = 0
-		for lineIndex in range(1, lineCount):
-			var lineBufferLength = len(get_line(lineIndex))
+		for lineIndex in range(lineCount):
+			var lineBufferLength = _font.get_string_size(get_line(lineIndex)).x
 			if maxLengthLineLength < lineBufferLength:
 				maxLengthLineLength = lineBufferLength
 				maxLengthLineIndex = lineIndex
 		# Inc/Dec width by the biggest line
-		var lineWidth = _font.get_string_size(get_line(maxLengthLineIndex)).x + 10
+		var lineWidth = maxLengthLineLength + 24
 		if (get_line_wrap_count(maxLengthLineIndex) > 0):
-			while (lineWidth > custom_minimum_size.x
-			and custom_minimum_size.x < _maxWidth):
-				custom_minimum_size.x += 1
+			while (lineWidth > size.x
+			and size.x < _maxWidth):
+				size.x += 1
 		else:
-			while (lineWidth < custom_minimum_size.x
-			and custom_minimum_size.x > _minWidth):
-				custom_minimum_size.x -= 1
+			while (lineWidth < size.x
+			and size.x > _minWidth):
+				size.x -= 1
+		size.y = lineCount * get_line_height()
+		if (sizeBefore != size):
+			resized.emit()
+
+func _ready():
+	_on_text_changed()
