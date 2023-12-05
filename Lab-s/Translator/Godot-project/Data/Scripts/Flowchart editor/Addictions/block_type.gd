fe511e6b9d28b2185b8c62b6b066e72eccb9a9b1
NemoNology <chicken.gang@mail.ru>
2023-12-05 16:48:58 +0900

    Added start text property for * in **
    * - flowchart block textedit
    ** flowchart block type class

--- Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/Addictions/block_type.gd
+++ Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/Addictions/block_type.gd
@@ -2,17 +2,22 @@ extends Node
 # Flowchart block type is:
 # 1) Flowchart block shape: FlowchartBlockShape
 # 2) Placeholder text: String
-# 3) Bonus code (see FlowchartBlock "Bonus code" property): Array
-class_name FlowchartBlocksType
+# 3) Start text: String
+# 4) Bonus code (see FlowchartBlock "Bonus code" property): Array
+class_name FlowchartBlockType
 
-var _shape: FlowChartBlockShape
+var _shape: FlowchartBlockShape
+var _startText: String
 var _placeholderText: String
 var _bonusCode: Array
 
 func _init(
-    shape: FlowChartBlockShape,
+    shape: FlowchartBlockShape,
     placeholderText: String,
-    bonusCode: Array):
+    bonusCode: Array,
+    startText: String = ""
+    ):
         _shape = shape
         _placeholderText = placeholderText
+        _startText = startText
         _bonusCode = bonusCode
