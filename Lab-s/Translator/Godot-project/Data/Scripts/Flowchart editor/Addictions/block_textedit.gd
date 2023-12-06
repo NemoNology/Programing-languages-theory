e911a07d93093c9261bef0be814801aca401902c
NemoNology <chicken.gang@mail.ru>
2023-12-07 03:24:51 +0900

    *Flowchart block text edit class
    Removed:
    - Build-in smart resizing functionality

--- Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/Addictions/block_textedit.gd
+++ Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/Addictions/block_textedit.gd
@@ -2,23 +2,17 @@ extends TextEdit
 
 class_name FlowchartBlockTextEdit
 
-var _maxWidth: int
-var _minWidth: int
-var _minHeight: int
+const DEFAULT_MIN_WIDTH = 80
+const DEFAULT_MIN_HEIGHT = 27
+
 var _font: Font
 
 func _init(
 	startText: String = "",
 	placeHolderText: String = "",
 	isEditable: bool = true,
-	maxWidth: int = 350,
-	minWidth: int = 80,
-	minHeight: int = 30
 	):
-	_minWidth = minWidth
-	_minHeight = minHeight
-	_maxWidth = maxWidth
-	custom_minimum_size = Vector2(_minWidth, _minHeight)
+	custom_minimum_size = Vector2(DEFAULT_MIN_WIDTH, DEFAULT_MIN_HEIGHT)
 	context_menu_enabled = false
 	scroll_fit_content_height = true
 	placeholder_text = placeHolderText
@@ -27,39 +21,8 @@ func _init(
 	add_theme_stylebox_override("normal", emptyStyleBox)
 	add_theme_stylebox_override("focus", emptyStyleBox)
 	add_theme_stylebox_override("read_only", emptyStyleBox)
-	text_changed.connect(_on_text_changed)
 	self_modulate = Color.WHITE
 	add_theme_color_override("selection_color", Color.LIGHT_GRAY)
 	_font = get_theme_font("font")
 	editable = isEditable
 	text = startText
-	
-func _ready():
-	_on_text_changed()
-	
-func _on_text_changed() -> void:
-	var lineCount = get_line_count()
-	var sizeBefore = size
-	# Change text edit width
-	if lineCount > 0:
-		# Finding the biggest line
-		var maxLengthLineLength: float = 0
-		var maxLengthLineIndex: int = 0
-		for lineIndex in range(lineCount):
-			var lineBufferLength = _font.get_string_size(get_line(lineIndex)).x
-			if maxLengthLineLength < lineBufferLength:
-				maxLengthLineLength = lineBufferLength
-				maxLengthLineIndex = lineIndex
-		# Inc/Dec width by the biggest line
-		var lineWidth = maxLengthLineLength + 24
-		if (get_line_wrap_count(maxLengthLineIndex) > 0):
-			while (lineWidth > size.x
-			and size.x < _maxWidth):
-				size.x += 1
-		else:
-			while (lineWidth < size.x
-			and size.x > _minWidth):
-				size.x -= 1
-		size.y = lineCount * get_line_height()
-		if (sizeBefore != size):
-			resized.emit()
