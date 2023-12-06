5883cb315780d32be7b0ffc9d6b0304d22ecf044
NemoNology <chicken.gang@mail.ru>
2023-12-06 21:52:35 +0900

    Created flowchart editor tool menu

---
+++ Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/tool_menu.gd
@@ -0,0 +1,10 @@
+extends PopupMenu
+
+class_name FlowchartToolMenu
+
+func _init():
+	add_item("Добавить блок \"Процесс\"")
+	add_item("Добавить блок \"Ввод с консоли\"")
+	add_item("Добавить блок \"Ввывод в консоль\"")
+	add_item("Добавить блок \"Условие\"")
+	add_item("Удалить выбранный(е) блоки")
