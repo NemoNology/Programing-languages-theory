3fef08942b9b60320e5baf7aae2c9726c2f04bb5
NemoNology <chicken.gang@mail.ru>
2023-12-08 18:19:14 +0900

    Flowchart editor class:
    Fixed:
    * Flowchart blocks checking method: condition (end) blocks check
    Improved:
    ^ Formatting
    ^ Flowchart blocks checking method: added
    1) Check all blocks connections;
    2) Blocks content emptiness
    ^ Code getting method (Not tested)

--- Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/editor.gd
+++ Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/editor.gd
@@ -2,214 +2,271 @@ extends GraphEdit
 
 class_name FlowchartEditor
 
-var blocksAmount: int = 1
-var toolMenu: FlowchartToolMenu
+var blocks_amount: int = 3
+var tool_menu: FlowchartToolMenu
+
 
 func _init():
 	show_grid = false
 	snapping_enabled = false
 	minimap_size = Vector2(120, 80)
-	var panelStyleBox: StyleBoxFlat = StyleBoxFlat.new()
-	panelStyleBox.bg_color = Color.GRAY
-	add_theme_stylebox_override("panel", panelStyleBox)
-	toolMenu = FlowchartToolMenu.new()
-	add_child(toolMenu)
-	toolMenu.id_pressed.connect(_on_toolMenu_id_pressed)
-	connection_request.connect(_on_connection_requested)
-	disconnection_request.connect(_on_disconnection_requested)
-	connection_to_empty.connect(_on_connected_to_empty)
-	delete_nodes_request.connect(_on_delete_nodes_requested)
+	var panel_style_box: StyleBoxFlat = StyleBoxFlat.new()
+	panel_style_box.bg_color = Color.GRAY
+	add_theme_stylebox_override("panel", panel_style_box)
+	tool_menu = FlowchartToolMenu.new()
+	add_child(tool_menu)
+	tool_menu.id_pressed.connect(on_tool_menu_id_pressed)
+	connection_request.connect(on_connection_requested)
+	disconnection_request.connect(on_disconnection_requested)
+	connection_to_empty.connect(on_connected_to_empty)
+	delete_nodes_request.connect(on_delete_nodes_requested)
 	right_disconnects = true
 
-func _on_toolMenu_id_pressed(id: int):
-	if (id <  toolMenu.item_count - 1):
+
+func on_tool_menu_id_pressed(id: int):
+	if id < tool_menu.item_count - 1:
 		var buffer: FlowchartBlock
 		match id:
-			0: buffer = FlowchartBlock.new(
-				blocksAmount,
-				FlowchartBlocksTypes.Process)
-			1: buffer = FlowchartBlock.new(
-				blocksAmount,
-				FlowchartBlocksTypes.HandInput)
-			2: buffer = FlowchartBlock.new(
-				blocksAmount,
-				FlowchartBlocksTypes.Output)
-			3: buffer = FlowchartBlock.new(
-				blocksAmount,
-				FlowchartBlocksTypes.ConditionIf)
-			4: buffer = FlowchartBlock.new(
-				blocksAmount,
-				FlowchartBlocksTypes.ConditionWhile)
-			5: buffer = FlowchartBlock.new(
-				blocksAmount,
-				FlowchartBlocksTypes.ConditionEnd) 
-		buffer.position_offset = toolMenu.position
-		blocksAmount += 1
+			0:
+				buffer = FlowchartBlock.new(blocks_amount, FlowchartBlocksTypes.Process)
+			1:
+				buffer = FlowchartBlock.new(blocks_amount, FlowchartBlocksTypes.HandInput)
+			2:
+				buffer = FlowchartBlock.new(blocks_amount, FlowchartBlocksTypes.Output)
+			3:
+				buffer = FlowchartBlock.new(blocks_amount, FlowchartBlocksTypes.ConditionIf)
+			4:
+				buffer = FlowchartBlock.new(blocks_amount, FlowchartBlocksTypes.ConditionWhile)
+			5:
+				buffer = FlowchartBlock.new(blocks_amount, FlowchartBlocksTypes.ConditionEnd)
+		buffer.position_offset = tool_menu.position
+		blocks_amount += 1
 		add_child(buffer)
-	elif (id == toolMenu.item_count - 1):
-		var childIndex: int = 0
-		while childIndex < get_child_count():
-			var childBuffer = get_child(childIndex)
-			if (childBuffer is FlowchartBlock
-				and childBuffer.selected
-				and childBuffer.input.editable
-				):
-				delete_nodes_request.emit([childBuffer.name])
-				remove_child(childBuffer)
-				blocksAmount -= 1
+	elif id == tool_menu.item_count - 1:
+		var child_index: int = 0
+		while child_index < get_child_count():
+			var child_buffer = get_child(child_index)
+			if (
+				child_buffer is FlowchartBlock
+				and child_buffer.selected
+				and child_buffer.input.editable
+			):
+				delete_nodes_request.emit([child_buffer.name])
+				remove_child(child_buffer)
 			else:
-				childIndex += 1
+				child_index += 1
 	grab_focus()
 
-func check_flowchart_blocks_connections() -> PackedStringArray:
+
+# Flowchart blocks checking
+# Check blocks connections
+# Check condition blocks closing by condition end blocks
+# Check empty blocks
+func check_flowchart_blocks() -> PackedStringArray:
 	var errors: PackedStringArray = []
 	var cons: Array[Dictionary] = get_connection_list()
-	var isThereNextBlock: bool
-	var areTherePreviousBlock: bool
-	var conditionsCheckStack: Array[int] = []
-	# Find connections not existing
-	# and check condition-blocks quiting/exiting/(...?)
+	var conditions_starts: Array[int] = []
+	var conditions_ends: Array[int] = []
+	var is_whitespace_regex = RegEx.new()
+	is_whitespace_regex.compile("^\\s*$")
 	for child in get_children():
-		if (child is FlowchartBlock
-			and child.id > 0):
-			isThereNextBlock = false
-			areTherePreviousBlock = false
-			if (child.type == FlowchartBlocksTypes.ConditionIf
+		if child is FlowchartBlock:
+			var is_there_next_block: bool = not child.is_slot_enabled_right(0)
+			var is_there_previous_block: bool = not child.is_slot_enabled_left(0)
+			var is_condition_block: bool = false
+			if (
+				child.type == FlowchartBlocksTypes.ConditionIf
 				or child.type == FlowchartBlocksTypes.ConditionWhile
-				):
-				conditionsCheckStack.push_back(child.id)
-			elif (child.type == FlowchartBlocksTypes.ConditionEnd):
-				if (conditionsCheckStack.size() > 0):
-					conditionsCheckStack.pop_back()
-				else: 
-					errors.append((
-						"Блок окончания условия %s является лишним"
-						% child.id))
+			):
+				conditions_starts.push_back(child.id)
+				is_condition_block = true
+			elif child.type == FlowchartBlocksTypes.ConditionEnd:
+				conditions_ends.push_back(child.id)
+			# Check block emptiness
+			if not child.type.flat and is_whitespace_regex.search(child.input.text):
+				errors.append("У блока %s отсутствует сожержимое" % child.id)
 			for con: Dictionary in cons:
-				if (isThereNextBlock and areTherePreviousBlock):
+				if is_there_next_block and is_there_previous_block:
 					break
-				elif (con["from_node"] == child.name
-					and con["from_port"] == 0):
-					isThereNextBlock = true
-				elif (con["to_node"] == child.name):
-					areTherePreviousBlock = true
-			if (not isThereNextBlock):
-				errors.append((
-					"От блока %s должен быть путь"
-					% child.id))
-			if (not areTherePreviousBlock):
-				errors.append((
-					"К блоку %s должен быть путь"
-					% child.id))
-	for blockID in conditionsCheckStack:
-		errors.append((
-			"Блок условия %s не заканчивается блоком окончания условия"
-			% blockID))
-
+				if con["from_node"] == child.name or is_there_next_block:
+					var to_node_type = get_node(NodePath(con["to_node"])).type
+					if con["from_port"] == 0:
+						is_there_next_block = true
+						if (
+							is_condition_block
+							and (to_node_type == FlowchartBlocksTypes.ConditionEnd)
+						):
+							(
+								errors
+								. append(
+									(
+										"Блок условия %s не содержит тела (и сразу же закрывается блоком конца условия)"
+										% child.id
+									)
+								)
+							)
+						elif (
+							child.type == FlowchartBlocksTypes.Begin
+							and to_node_type == FlowchartBlocksTypes.End
+						):
+							errors.append("Тело блок-схемы не сожержит алгоритм")
+					else:
+						if (
+							is_condition_block
+							and (to_node_type == FlowchartBlocksTypes.ConditionEnd)
+						):
+							conditions_starts.push_back(child.id)
+							(
+								errors
+								. append(
+									(
+										"Тело (ветвь) \'иначе\' блока условия %s является пустым (и сразу же закрывается блоком конца условия)"
+										% child.id
+									)
+								)
+							)
+				is_there_previous_block = con["to_node"] == child.name or is_there_previous_block
+			if not is_there_next_block:
+				errors.append("Блок %s не имеет продолжения" % child.id)
+			if not is_there_previous_block:
+				errors.append("Блок %s не имеет начала" % child.id)
+	# Check condition block
+	while conditions_starts.size() > 0:
+		if conditions_ends.size() > 0:
+			conditions_starts.pop_back()
+			conditions_ends.pop_back()
+		else:
+			for block_id in conditions_starts:
+				errors.append("Блок условия %s не закрыт блоком конца условия" % block_id)
+			break
+	# Check condition end block
+	for block_id in conditions_ends:
+		errors.append("Блок конца условия %s закрывает несуществующий блок условия" % block_id)
 	return errors
+
 
 func get_code() -> Dictionary:
 	var code: Dictionary = {}
-	var tabulatesCount: int = 0
-	var childAmount = get_child_count()
-	for childIndex in range(childAmount):
-		var child: Node = get_child(childIndex)
-		if (child is FlowchartBlock
-			and child.id > 0
-			):
-			if (child.type == FlowchartBlocksTypes.ConditionIf
-				or child.type == FlowchartBlocksTypes.ConditionWhile
-				):
-				tabulatesCount += 1
-			elif (tabulatesCount > 1
-				and childIndex + 1 < childAmount
-				):
-				var nextBlockType = get_child(childIndex + 1).type
-				if (nextBlockType != FlowchartBlocksTypes.ConditionIf
-					and nextBlockType != FlowchartBlocksTypes.ConditionWhile
-					):
-					tabulatesCount -= 1
-			code[child.id] = child.get_code(tabulatesCount)
+	var tabulates_count: int = 0
+	var branches_queue: Array[FlowchartBlock] = []
+	# Find and starts from begin block
+	for child in get_children():
+		if child is FlowchartBlock and child.type == FlowchartBlocksTypes.Begin:
+			branches_queue.push_back(child)
+			break
+	# Go on every branch
+	# TODO: code getting
+	var cons: Array[Dictionary] = get_connection_list()
+	while branches_queue.size() > 0:
+		var block_buffer: FlowchartBlock = branches_queue.pop_front()
+		if block_buffer.type == FlowchartBlocksTypes.End:
+			return code
+		for con in cons:
+			if con["from_node"] == block_buffer.name:
+				if block_buffer.type == FlowchartBlocksTypes.ConditionWhile:
+					tabulates_count += 1
+					block_buffer = get_node(NodePath(con["to_node"]))
+				elif block_buffer.type == FlowchartBlocksTypes.ConditionIf:
+					if con["from_port"] == 0:
+						block_buffer = get_node(NodePath(con["to_node"]))
+						# Searching 'else' block
+						for con1 in cons:
+							if con1["from_node"] == con["from_node"] and con1["from_port"] == 1:
+								branches_queue.push_back(get_node(NodePath(con1["to_node"])))
+								break
+					else:
+						branches_queue.push_back(get_node(NodePath(con["to_node"])))
+						# Searching 'then' block
+						for con1 in cons:
+							if con1["from_node"] == con["from_node"] and con1["from_port"] == 0:
+								block_buffer = get_node(NodePath(con1["to_node"]))
+								break
+				elif block_buffer.type == FlowchartBlocksTypes.ConditionEnd:
+					tabulates_count -= 1
+					block_buffer = get_node(NodePath(con["to_node"]))
+				else:
+					block_buffer = get_node(NodePath(con["to_node"]))
+				break
+		code[block_buffer.id] = block_buffer.get_code(tabulates_count)
+
 	return code
 
+
 func _gui_input(event):
-	if (event is InputEventMouseButton):
-		if (event.pressed):
-			if (event.button_index == MOUSE_BUTTON_LEFT):
-				toolMenu.hide()
-			elif (event.button_index == MOUSE_BUTTON_RIGHT):
-				toolMenu.position = event.global_position
-				toolMenu.show()
-	elif (event is InputEventKey):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				tool_menu.hide()
+			elif event.button_index == MOUSE_BUTTON_RIGHT:
+				tool_menu.position = event.global_position
+				tool_menu.show()
+	elif event is InputEventKey:
 		var kc: Key = event.keycode
-		if (kc == KEY_A):
+		if kc == KEY_A:
 			for child in get_children():
-				if (child is FlowchartBlock
-					and child.input.editable
-					):
+				if child is FlowchartBlock and child.input.editable:
 					child.selected = true
-		elif (kc == KEY_X or kc == KEY_DELETE):
-			_on_toolMenu_id_pressed(toolMenu.item_count - 1)
+		elif kc == KEY_X or kc == KEY_DELETE:
+			on_tool_menu_id_pressed(tool_menu.item_count - 1)
+
 
 func _ready():
 	anchors_preset = PRESET_FULL_RECT
-	var beginBlock: FlowchartBlock = FlowchartBlock.new(
-		0, FlowchartBlocksTypes.Begin, false)
-	beginBlock.position_offset = Vector2(
-		200,
-		120 - beginBlock.size.y)
-	var endBlock: FlowchartBlock = FlowchartBlock.new(
-		0, FlowchartBlocksTypes.End, false)
-	endBlock.position_offset = Vector2(
-		400 - endBlock.size.x,
-		beginBlock.position_offset.y)
-	add_child(beginBlock)
-	add_child(endBlock)
+	var begin_block: FlowchartBlock = FlowchartBlock.new(1, FlowchartBlocksTypes.Begin)
+	begin_block.position_offset = Vector2(200, 120 - begin_block.size.y)
+	var end_block: FlowchartBlock = FlowchartBlock.new(2, FlowchartBlocksTypes.End)
+	end_block.position_offset = Vector2(400 - end_block.size.x, begin_block.position_offset.y)
+	add_child(begin_block)
+	add_child(end_block)
 
-func _on_connection_requested(
-	from_node: StringName, from_port: int, 
-	to_node: StringName, to_port: int
-	):
-	if (from_node != to_node):
+
+func on_connection_requested(
+	from_node: StringName, from_port: int, to_node: StringName, to_port: int
+):
+	if from_node != to_node:
 		for con in get_connection_list():
-			if (con["from_node"] == from_node
-				and con["from_port"] == from_port
-				):
-				disconnection_request.emit(
-					from_node, from_port,
-					con["to_node"], con["to_port"]
-				)
+			if con["from_node"] == from_node and con["from_port"] == from_port:
+				disconnection_request.emit(from_node, from_port, con["to_node"], con["to_port"])
 		connect_node(
-			from_node, from_port,
-			to_node, to_port,
-			)
+			from_node,
+			from_port,
+			to_node,
+			to_port,
+		)
 
-func _on_disconnection_requested(
-	from_node: StringName, from_port: int, 
-	to_node: StringName, to_port: int
-	):
+
+func on_disconnection_requested(
+	from_node: StringName, from_port: int, to_node: StringName, to_port: int
+):
 	disconnect_node(
-		from_node, from_port,
-		to_node, to_port,
+		from_node,
+		from_port,
+		to_node,
+		to_port,
 	)
 
-func _on_connected_to_empty(from_node: StringName, from_port: int, release_position: Vector2):
+
+func on_connected_to_empty(from_node: StringName, from_port: int, release_position: Vector2):
 	for con in get_connection_list():
-		if (con["from_node"] == from_node
-			and con["from_port"] == from_port
-			):
+		if con["from_node"] == from_node and con["from_port"] == from_port:
 			disconnect_node(
-				from_node, from_port,
-				con["to_node"], con["to_port"],
+				from_node,
+				from_port,
+				con["to_node"],
+				con["to_port"],
 			)
 
-func _on_delete_nodes_requested(nodes: Array):
+
+func on_delete_nodes_requested(nodes: Array):
 	for node in nodes:
 		for con in get_connection_list():
-			if (con["from_node"] == node
-				or con["to_node"] == node
-				):
-				disconnection_request.emit(
-					con["from_node"], con["from_port"],
-					con["to_node"], con["to_port"],
+			if con["from_node"] == node or con["to_node"] == node:
+				(
+					disconnection_request
+					. emit(
+						con["from_node"],
+						con["from_port"],
+						con["to_node"],
+						con["to_port"],
+					)
 				)
