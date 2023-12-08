b7e1572ea733964409d73e141c7dc9d06d90c0cc
NemoNology <chicken.gang@mail.ru>
2023-12-09 00:26:31 +0900

    Flowchart editor tool menu class:
    Added:
    + Item ID getting by static var-s
    + Select all blocks option

--- Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/tool_menu.gd
+++ Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/tool_menu.gd
@@ -2,6 +2,18 @@ extends PopupMenu
 
 class_name FlowchartToolMenu
 
+## Dictionary: key - menu option/item ID; Value - 
+static var AddingBlockTypeByOptionID: Dictionary = {
+	0: FlowchartBlocksTypes.Process,
+	1: FlowchartBlocksTypes.HandInput,
+	2: FlowchartBlocksTypes.Output,
+	3: FlowchartBlocksTypes.ConditionIf,
+	4: FlowchartBlocksTypes.ConditionWhile,
+	5: FlowchartBlocksTypes.ConditionEnd,
+}
+static var DeleteSelectedBlocksOptionID: int = 8
+static var SelectAllBlocksOptionID: int = 7
+
 func _init():
 	add_item("Добавить блок \"Процесс\"")
 	add_item("Добавить блок \"Ввод с консоли\"")
@@ -9,4 +21,7 @@ func _init():
 	add_item("Добавить блок \"Условный оператор\"")
 	add_item("Добавить блок \"Цикл\"")
 	add_item("Добавить блок \"Конец условия\"")
-	add_item("Удалить выбранные блоки")
+	add_separator()
+	add_item("Выделить все блоки")
+	add_item("Удалить выделенные блоки")
+	add_separator()
