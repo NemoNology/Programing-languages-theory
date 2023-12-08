b527eeebec7a5b60885e3b3e5bffa535e014cb84
NemoNology <chicken.gang@mail.ru>
2023-12-08 13:58:38 +0900

    File: formatted

--- Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/Addictions/block_shape.gd
+++ Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/Addictions/block_shape.gd
@@ -4,18 +4,22 @@ class_name FlowchartBlockShape
 
 var _points: PackedVector2Array
 
+
 func _init(shapePoints: PackedVector2Array, color: Color):
 	_points = shapePoints
 	default_color = color
 	closed = true
 	width = 5
 
+
 static func new_from(shape: FlowchartBlockShape) -> FlowchartBlockShape:
 	return FlowchartBlockShape.new(shape._points, shape.default_color)
-	
+
+
 func _ready():
 	_on_resized()
 	get_parent().resized.connect(_on_resized)
+
 
 func _on_resized():
 	var parent: Node = get_parent()
@@ -23,10 +27,9 @@ func _on_resized():
 	var newY = parent.size.y
 	var pointsBuffer: PackedVector2Array = []
 	for point in _points:
-		pointsBuffer.append(Vector2(
-			point.x * newX,
-			point.y * newY))
+		pointsBuffer.append(Vector2(point.x * newX, point.y * newY))
 	points = pointsBuffer
+
 
 func _draw():
 	draw_polyline(points, default_color, width, antialiased)
