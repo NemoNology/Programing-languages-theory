70ee47ee4320ad35ef414d5a630ffe2df8fbad77
NemoNology <chicken.gang@mail.ru>
2023-12-08 15:30:50 +0900

    Flowchart block type class:
    Fixed:
    * Class description
    Improved:
    ^ Formatting

--- Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/Addictions/block_type.gd
+++ Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/Addictions/block_type.gd
@@ -2,26 +2,27 @@ extends Node
 # Flowchart block type is:
 # 1) Flowchart block shape: FlowchartBlockShape
 # 2) Placeholder text: String
-# 3) Start text: String
-# 4) Bonus code (see FlowchartBlock "Bonus code" property): Array
+# 3) Tooltip text: String
+# 4) (Is) Flat (block): bool
+# 5) Slots: Array[FlowchartBlockSlot]
 class_name FlowchartBlockType
 
 var shape: FlowchartBlockShape
-var startText: String
 var placeholderText: String
 var tooltipText: String
 var flat: bool
-var slots: Array
+var slots: Array[FlowchartBlockSlot]
+
 
 func _init(
-    block_shape: FlowchartBlockShape,
-    block_placeholderText: String,
-    block_tooltip_text: String,
-    block_slots: Array = [FlowchartBlockSlot.DefautSlot],
-    is_flat: bool = false,
-    ):
-        shape = block_shape
-        placeholderText = block_placeholderText
-        slots = block_slots
-        tooltipText = block_tooltip_text
-        flat = is_flat
+	block_shape: FlowchartBlockShape,
+	block_placeholderText: String,
+	block_tooltip_text: String,
+	block_slots: Array = [FlowchartBlockSlot.DefautSlot],
+	is_flat: bool = false,
+):
+	shape = block_shape
+	placeholderText = block_placeholderText
+	slots = block_slots
+	tooltipText = block_tooltip_text
+	flat = is_flat
