708dd602834556cd9ad011adfda9441acde3d89c
NemoNology <chicken.gang@mail.ru>
2023-12-09 23:28:13 +0900

    Flowchart block text edit class:
    Improved:
    ^ View

--- Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/Addictions/block_textedit.gd
+++ Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/Addictions/block_textedit.gd
@@ -9,12 +9,6 @@ const DEFAULT_MIN_HEIGHT = 27
 func _init(
 	is_flat: bool = false,
 ):
-	context_menu_enabled = false
-	var emptyStyleBox: StyleBoxEmpty = StyleBoxEmpty.new()
-	add_theme_stylebox_override("normal", emptyStyleBox)
-	add_theme_stylebox_override("focus", emptyStyleBox)
-	add_theme_stylebox_override("read_only", emptyStyleBox)
-
 	if is_flat:
 		custom_minimum_size = Vector2.ZERO
 	else:
@@ -22,7 +16,14 @@ func _init(
 		placeholder_text = "..."
 		wrap_mode = TextEdit.LINE_WRAPPING_BOUNDARY
 		scroll_fit_content_height = true
-		add_theme_color_override("selection_color", Color.GRAY)
-		add_theme_color_override("font_color", Color.BLACK)
-		add_theme_color_override("font_placeholder_color", Color.DIM_GRAY)
 
+	var style_box_empty: StyleBoxEmpty = StyleBoxEmpty.new()
+	add_theme_stylebox_override("normal", style_box_empty)
+	add_theme_stylebox_override("focus", style_box_empty)
+	add_theme_stylebox_override("read_only", style_box_empty)
+
+	add_theme_color_override("font_color", Color.BLACK)
+	add_theme_color_override("font_placeholder_color", Color.DIM_GRAY)
+	add_theme_color_override("selection_color", Color.LIGHT_GRAY)
+	add_theme_color_override("caret_color", Color.BLACK)
+	add_theme_color_override("current_line_color", Color(Color.BLACK, 0.5))
