6cfc3dc7ef962eecffc1856ad1bb1a1cd082ba52
NemoNology <chicken.gang@mail.ru>
2023-12-09 22:45:01 +0900

    Flowchart block class:
    Improved:
    ^ Title -> now it display block type name
    ^ Second slot title -> more readeble view
    ^ Block view
    ^ Shape view -> now shape is visible (again ^_^)

--- Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/block.gd
+++ Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/block.gd
@@ -11,40 +11,49 @@ var type: FlowchartBlockType
 var input: FlowchartBlockTextEdit
 var shape: FlowchartBlockShape
 
-const CODE_INPUT_BLOCK_MARGIN = 15
+const CODE_INPUT_BLOCK_MARGIN = 10
 
 
 func _init(block_id: int, block_type: FlowchartBlockType):
 	id = block_id
 	type = block_type
-	input = (
-		FlowchartBlockTextEdit
-		. new(
-			block_type.placeholderText,
-			block_type.flat,
-		)
-	)
-	title = str(id)
-	if block_type.flat:
-		title += ": " + block_type.placeholderText
-	else:
-		resizable = true
-	shape = block_type.shape
+	input = (FlowchartBlockTextEdit.new(block_type.flat))
+	shape = block_type.shape.new_copy()
+
+	title = "%s: %s" % [str(id), block_type.type_name]
 	tooltip_text = block_type.tooltipText
 	input.tooltip_text = tooltip_text
 	add_child(input)
-	set_anchors_preset(PRESET_CENTER)
-	var titlebar_style_box = StyleBoxFlat.new()
-	var titlebar_selected_style_box = StyleBoxFlat.new()
-	titlebar_style_box.bg_color = Color(shape.color, 0.5)
-	titlebar_selected_style_box.bg_color = shape.color
+
+	var panel_style_box: StyleBox = get_theme_stylebox("panel")
+	var panel_selected_style_box: StyleBox = get_theme_stylebox("panel_selected")
+	if type.flat:
+		panel_style_box = StyleBoxEmpty.new()
+		panel_selected_style_box = panel_style_box
+	else:
+		panel_style_box.bg_color = Color.TRANSPARENT
+		panel_selected_style_box.bg_color = Color.TRANSPARENT
+		resizable = true
+	var titlebar_style_box: StyleBoxFlat = StyleBoxFlat.new()
+	titlebar_style_box.bg_color = shape.color.darkened(0.2)
+	var titlebar_selected_style_box: StyleBoxFlat = StyleBoxFlat.new()
+	titlebar_selected_style_box.bg_color = shape.default_color
 	add_theme_stylebox_override("titlebar", titlebar_style_box)
 	add_theme_stylebox_override("titlebar_selected", titlebar_selected_style_box)
+	add_theme_stylebox_override("panel", panel_style_box)
+	add_theme_stylebox_override("panel_selected", panel_selected_style_box)
+
+	node_selected.connect(_on_node_selected)
+	node_deselected.connect(_on_node_deselected)
+
 	for port_index in range(type.slots.size()):
 		var slot: FlowchartBlockSlot = type.slots[port_index]
 		if port_index > 0:
 			var label_buffer: Label = Label.new()
 			label_buffer.add_theme_font_size_override("font_size", 8)
+			label_buffer.add_theme_color_override("font_color", Color.BLACK)
+			label_buffer.size_flags_horizontal = Control.SIZE_FILL
+			label_buffer.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
 			label_buffer.text = slot.text
 			label_buffer.uppercase = true
 			add_child(label_buffer)
@@ -58,20 +67,37 @@ func _init(block_id: int, block_type: FlowchartBlockType):
 			FlowchartBlockSlot.DEFAULT_OUT_PORT_COLOR
 		)
 
+	add_child(shape)
+
 
 func get_code(tabulatesCount: int = 0) -> String:
 	var codeBuffer: String
 	codeBuffer = input.text.replace("\n", "\n" + " tab ".repeat(tabulatesCount))
 	if type == FlowchartBlocksTypes.HandInput:
-		return codeBuffer + " будет ввод."
+		return codeBuffer + " будет ввод. "
 	elif type == FlowchartBlocksTypes.Output:
-		return "вывод " + codeBuffer + "."
+		return " вывод " + codeBuffer + "."
 	elif type == FlowchartBlocksTypes.ConditionIf:
-		return "если ( " + codeBuffer + " ) : "
+		return " если ( " + codeBuffer + " ) . "
 	elif type == FlowchartBlocksTypes.ConditionWhile:
-		return "пока ( " + codeBuffer + " ) : "
+		return " пока ( " + codeBuffer + " ) . "
+	elif type == FlowchartBlocksTypes.ConditionEnd:
+		return " конец. " + codeBuffer
 	return codeBuffer
 
 
 func _ready():
 	input.position = Vector2(CODE_INPUT_BLOCK_MARGIN, CODE_INPUT_BLOCK_MARGIN)
+	node_deselected.emit()
+
+
+func _on_node_selected():
+	shape.color = shape.default_color
+
+
+func _on_node_deselected():
+	shape.color = shape.default_color.darkened(0.2)
+
+
+func _draw():
+	shape.queue_redraw()
