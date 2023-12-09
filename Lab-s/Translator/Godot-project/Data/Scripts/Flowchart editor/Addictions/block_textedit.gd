3193f40935cf9c60b9ed6f7213d89decb45e8941
NemoNology <chicken.gang@mail.ru>
2023-12-09 20:30:12 +0900

    Flowchart block text edit class:
    Removed:
    - Extra input parameter -> placeholder text, as placeholder text always is '...'
    Changed:
    > Selection color
    > Foregroung color for placeholder and inputted text

--- Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/Addictions/block_textedit.gd
+++ Lab-s/Translator/Godot-project/Data/Scripts/Flowchart editor/Addictions/block_textedit.gd
@@ -7,7 +7,6 @@ const DEFAULT_MIN_HEIGHT = 27
 
 
 func _init(
-	block_placeholder_text: String = "",
 	is_flat: bool = false,
 ):
 	context_menu_enabled = false
@@ -20,8 +19,10 @@ func _init(
 		custom_minimum_size = Vector2.ZERO
 	else:
 		custom_minimum_size = Vector2(DEFAULT_MIN_WIDTH, DEFAULT_MIN_HEIGHT)
-		placeholder_text = block_placeholder_text
+		placeholder_text = "..."
 		wrap_mode = TextEdit.LINE_WRAPPING_BOUNDARY
 		scroll_fit_content_height = true
-		self_modulate = Color.WHITE
-		add_theme_color_override("selection_color", Color.LIGHT_GRAY)
+		add_theme_color_override("selection_color", Color.GRAY)
+		add_theme_color_override("font_color", Color.BLACK)
+		add_theme_color_override("font_placeholder_color", Color.DIM_GRAY)
+
