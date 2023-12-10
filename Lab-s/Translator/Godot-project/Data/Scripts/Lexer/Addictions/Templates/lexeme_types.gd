6c09f177ad920c30df6f604d43c5395ac5dc12f6
NemoNology <chicken.gang@mail.ru>
2023-12-11 02:23:17 +0900

    Added lexeme types templates

---
+++ Lab-s/Translator/Godot-project/Data/Scripts/Lexer/Addictions/Templates/lexeme_types.gd
@@ -0,0 +1,56 @@
+extends Object
+
+class_name LexemeTypes
+
+## Console out
+static var Cout: String = "вывод"
+## Console in
+static var Cin: String = "ввод"
+## Assign (variable to value)
+static var Assign: String = "будет"
+## Open brackets
+static var Obr: String = "("
+## Close brackets
+static var Cbr: String = ")"
+## Separator - '.'
+static var Separatop: String = ")"
+## If - conditional operator
+static var If: String = "если"
+## While - loop
+static var While: String = "пока"
+## Else - conditional operator second body
+static var Else: String = "иначе"
+## End - end of if/while
+static var End: String = "конец"
+## Adding - operator
+static var Add: String = "плюс"
+## Subtraction - operator
+static var Sub: String = "минус"
+## Multiplication - operator
+static var Mul: String = "умножить"
+## Division - operator
+static var Div: String = "разделить"
+## False
+static var False: String = "ложь"
+## True
+static var True: String = "правда"
+## Not - logical operator
+static var Not: String = "не"
+## And - logical operator
+static var And: String = "и"
+## Or - logical operator
+static var Or: String = "или"
+## More (5 is more than 3)
+static var More: String = "больше"
+## Less (3 is less than 5)
+static var Less: String = "меньше"
+## Equals (4 is equal 2 + 2)
+static var Equals: String = "равно"
+## Variable - variable ID
+static var Var: String = "переменная"
+## Integer
+static var Int: String = "целочисленное"
+## String
+static var StringType: String = "строка"
+## Unknown lexeme type
+static var Unknown: String = "неизвестно"
