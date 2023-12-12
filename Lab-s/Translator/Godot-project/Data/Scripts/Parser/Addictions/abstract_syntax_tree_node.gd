a04bea409206bb364e19eb553616f41da4607ac6
NemoNology <chicken.gang@mail.ru>
2023-12-13 07:30:03 +0900

    Added abstract syntax tree node class

---
+++ Lab-s/Translator/Godot-project/Data/Scripts/Parser/Addictions/abstract_syntax_tree_node.gd
@@ -0,0 +1,15 @@
+class_name AbstractSyntaxTreeNode
+
+var parsed_value = null
+var parsed_value_type = null
+var left: AbstractSyntaxTreeNode
+var right: AbstractSyntaxTreeNode
+
+
+func _init(left_node: AbstractSyntaxTreeNode = null, right_node: AbstractSyntaxTreeNode = null):
+	left = left_node
+	right = right_node
+
+
+func parse():
+	pass
