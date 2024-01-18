7c0aa4deb8ecc6e2d5d282fc478bcc2915f27403
NemoNology <chicken.gang@mail.ru>
2024-01-18 12:26:55 +0900

    Error class
    Changed:
    > Static var-s to const var-s

--- Lab-s/Translator/Godot-project/Data/Scripts/Other/error.gd
+++ Lab-s/Translator/Godot-project/Data/Scripts/Other/error.gd
@@ -19,28 +19,37 @@ func _to_string():
 			"символ %s" % position.z,
 		]
 	)
-	
 
-static func get_wrong_operand_type_error(expected_types: PackedStringArray, getted_type: String, error_position: Vector3i):
+
+static func get_wrong_operand_type_error(
+	expected_types: PackedStringArray, getted_type: String, error_position: Vector3i
+):
 	var error_text: String = "%s: ожидалось " % WrongOperandTypeErrorText
 	for type in expected_types:
 		error_text += type.to_lower() + ", "
-	return Error.new(error_text.substr(0, error_text.length() - 3) + ", но получен %s" % getted_type, error_position)
-	
+	return Error.new(
+		error_text.substr(0, error_text.length() - 3) + ", но получен %s" % getted_type,
+		error_position
+	)
 
-static var UnknownLexemeErrorText: String = "Неизвестная лексема"
-static var EmptyBlockErrorText: String = "У блока отсутствует сожержимое"
-static var EmptyConditionBlockBodyErrorText: String = "Блок условия не содержит тела"
-static var EmptyFlowchartErrorText: String = "Пустая блок-схема"
-static var EmptyElseBodyErrorText: String = "Пустое тело 'иначе' блока условного оператора"
-static var NoBlockContinuationErrorText: String = "Блок не имеет продолжения"
-static var NoBlockBeginningErrorText: String = "Блок не имеет начала"
-static var ExcessEndConditionBlockErrorText: String = "Блок конца условия закрывает несуществующий блок условия"
-static var ExcessElseBlockErrorText: String = "Ожидалось начало условного оператора перед '%s'" % LexemeTypes.Else
-static var ExcessBracketsClosingErrorText: String = "Попытка закрыть несуществующую скобку"
-static var ExcessBracketsOpeningErrorText: String = "Незакрытая скобка"
-static var NoOperandsErrorText: String = "Отсутсвует(ют) операнд(ы)"
-static var UnknownOperationErrorText: String = "Неизвестная операция"
-static var UnknownVariableErrorText: String = "Неизвестная переменная"
-static var WrongOperandTypeErrorText: String = "Неверный тип операнда(ов)"
-static var DivisionByZeroTypeErrorText: String = "Деление на ноль"
+
+const UnknownLexemeErrorText: String = "Неизвестная лексема"
+const EmptyBlockErrorText: String = "У блока отсутствует сожержимое"
+const EmptyConditionBlockBodyErrorText: String = "Блок условия не содержит тела"
+const EmptyFlowchartErrorText: String = "Пустая блок-схема"
+const EmptyElseBodyErrorText: String = "Пустое тело 'иначе' блока условного оператора"
+const NoBlockContinuationErrorText: String = "Блок не имеет продолжения"
+const NoBlockBeginningErrorText: String = "Блок не имеет начала"
+const ExcessEndConditionBlockErrorText: String = "Блок конца условия закрывает несуществующий блок условия"
+static var ExcessElseBlockErrorText: String = (
+	"Ожидалось начало условного оператора перед '%s'" % LexemeTypes.Else
+)
+const ExcessBracketsClosingErrorText: String = "Попытка закрыть несуществующую скобку"
+const ExcessBracketsOpeningErrorText: String = "Незакрытая скобка"
+const NoOperandsErrorText: String = "Отсутсвует(ют) операнд(ы)"
+const NoOperatorsErrorText: String = "Отсутсвует(ют) операторы(ы)"
+const UnknownOperatorErrorText: String = "Неизвестная операция"
+const UnknownVariableErrorText: String = "Неизвестная переменная"
+const WrongOperandTypeErrorText: String = "Неверный тип операнда(ов)"
+const DivisionByZeroTypeErrorText: String = "Деление на ноль"
+const ExcessOperandsErrorText: String = "Избыток операндов"
