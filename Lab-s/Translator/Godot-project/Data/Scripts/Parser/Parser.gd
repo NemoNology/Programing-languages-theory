f8ae9ff96f7f18755aca7596990c171ce89b11e2
NemoNology <chicken.gang@mail.ru>
2024-01-18 12:33:58 +0900

    Completed parser - B 1.0

--- Lab-s/Translator/Godot-project/Data/Scripts/Parser/Parser.gd
+++ Lab-s/Translator/Godot-project/Data/Scripts/Parser/Parser.gd
@@ -33,31 +33,75 @@ static func split_to_branches(lexemes: Array[Lexeme]) -> Array[Array]:
 static func parse_branch(
 	branch: Array[Lexeme], out_parsing_result: ParsingResult, out_then_else_stack: Array[bool]
 ) -> bool:
-	var brackets_stack: Array[Lexeme] = []
+	var brackets_stack: Array[Vector3i] = []
 	var operands: Array[Operand] = []
-	var operators: Array[Operation] = []
+	var operators: Array[Operator] = []
 	for l in branch:
 		if l.type == LexemeTypes.If:
 			out_then_else_stack.push_back(true)
 		elif l.type == LexemeTypes.Else:
 			if out_then_else_stack.size() < 1:
-				out_parsing_result.errors.append(Error.new(Error.ExcessElseBlockError, l.position))
-				# TODO: Maybe 'continue' instead of 'return false'?
+				out_parsing_result.errors.append(
+					Error.new(Error.ExcessElseBlockErrorText, l.position)
+				)
 				return false
 			out_then_else_stack.pop_back()
 		elif l.type == LexemeTypes.Obr:
-			brackets_stack.push_back(l)
+			brackets_stack.push_back(l.position)
 		elif l.type == LexemeTypes.Cbr:
 			if brackets_stack.size() < 1:
-				out_parsing_result.errors.append(Error.new(Error.ExcessBracketsClosingError, l.position))
+				out_parsing_result.errors.append(
+					Error.new(Error.ExcessBracketsClosingErrorText, l.position)
+				)
 				return false
 			brackets_stack.pop_back()
 
-		# if l.type != 
-
 	if brackets_stack.size() > 0:
-		for br: Lexeme in brackets_stack:
-			out_parsing_result.errors.append(Error.new(Error.ExcessBracketsOpeningError, br.position))
+		for br_pos: Vector3i in brackets_stack:
+			out_parsing_result.errors.append(
+				Error.new(Error.ExcessBracketsOpeningErrorText, br_pos)
+			)
+		return false
+
+	# Calculating
+	for l in branch:
+		if l.type in Operand.OperandsLexemeTypes:
+			operands.push_back(Operand.init_from_lexeme(l))
+		elif l.type in Operator.OperatorsLexemeTypes:
+			var operator_buffer: Operator = Operator.init_by_lexeme(l, out_parsing_result)
+			if (
+				operators.size() < 1
+				or operator_buffer is ObrOperator
+				or operator_buffer.priority <= operators[-1].priority
+			):
+				operators.append(operator_buffer)
+			elif operator_buffer is CbrOperator:
+				while not (operator_buffer is ObrOperator):
+					if operators.size() < 1:
+						var pos: Vector3i = operator_buffer.position
+						if operands.size() > 1:
+							pos = operands[-1].position
+						out_parsing_result.errors.append(Error.new(Error.NoOperatorsErrorText, pos))
+						return false
+					if not operators.pop_back().try_calculate(operands, out_parsing_result):
+						return false
+				# Removing found open bracket
+				operators.pop_back()
+			elif operator_buffer.priority > operators[-1].priority:
+				if not operators.pop_back().try_calculate(operands, out_parsing_result):
+					return false
+				operators.push_back(operator_buffer)
+		else:
+			out_parsing_result.errors.append(Error.new(Error.UnknownLexemeErrorText, l.position))
+			return false
+
+	while operators.size() > 0:
+		if not operators.pop_back().try_calculate(operands, out_parsing_result):
+			return false
+
+	if operands.size() > 0:
+		for o: Operand in operands:
+			out_parsing_result.errors.append(Error.new(Error.ExcessOperandsErrorText, o.position))
 		return false
 
 	return true
